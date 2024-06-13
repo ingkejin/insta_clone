6fe4c8b2fb1cd3159a357f09653a642069269a15
ingkejin <ingkejin@gmail.com>
2024-06-13 11:14:58 +0900

    ✨ mode Toggle 삼항연산자로 구현

--- script.js
+++ script.js
@@ -68,3 +68,35 @@ function pwMode() {
 }
 
 pwVisible.addEventListener('click', pwMode)
+
+
+
+// Darkmode / Lightmode
+let modeBtn = document.getElementById('mode-toggle');
+
+function modeToggle(e) {
+  e.preventDefault();
+  // 1. body 태그에 dark 라는 class toggle
+  // dark 라는 class가 있으면 remove, 없으면 add
+  let body = document.querySelector('body');
+  body.classList.toggle('dark');
+
+  // body에 dark라는 클래스가 있을 때
+  // modeBtn.innerHTML = 'Lightmode'
+  // classList.contans()
+
+
+  if(body.classList.contains('dark')) {
+    modeBtn.innerHTML = 'Lightmode';
+ } else {
+   // body에 dark라는 클래스가 없을 때
+   // modeBtn.innerHTML = "Darkmode"
+   modeBtn.innerHTML = 'Darkmode';
+ }
+
+  // 삼항연산자
+  // 조건 ? 참일 때 : 거짓일 때
+  // modeBtn.innerHTML = body.classList.contains('dark') ? 'Lightmode' : 'Darkmode';
+}
+
+modeBtn.addEventListener('click', modeToggle);
