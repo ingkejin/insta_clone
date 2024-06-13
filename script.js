37d5689f545dc3ab765bd90c72b152fd82dd1130
ingkejin <ingkejin@gmail.com>
2024-06-13 09:33:27 +0900

    📝 e.target 설명

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function updateInputState(input, activeVar) {
   // trim() 공백
-  // 공백 제거한 value 값의 길이가 0보다 클 때
+  // 공백 제거한 value 값의 길이가 0보다 클 때 => 사용자가 input 창에 입력한 값이 하나라도 있냐
   if(input.value.trim().length > 0) {
     input.parentElement.classList.add('active');
     activeVar = true;
@@ -10,7 +10,8 @@ function updateInputState(input, activeVar) {
     activeVar = false;
   }
 
-  return activeVar;
+  // return : 해당 함수가 실행된 후, 함수 호출한 위치로 전달되는 값
+  return activeVar; // true , false
 }
 
 let userId = document.getElementById('userid');
@@ -28,17 +29,21 @@ function handleInput(e) {
   let type = input.getAttribute("type");
   
   if(type == "text") {
-    idActive = updateInputState(input, idActive);
+    idActive = updateInputState(input, idActive); // return 값 => true, false
   } else {
-    pwActive = updateInputState(input, pwActive);
+    // type이 text가 아닐 경우 => password
+    pwActive = updateInputState(input, pwActive); // return 값 => true, false
   }
 
+  // idActive랑 pwActive가 모두 참일 때 => input.value.length > 0
   if(idActive && pwActive) {
     submitBtn.removeAttribute('disabled');
   } else {
+    // idActive나 pwActive 중 둘 중 하나라도 false 일 때 
     submitBtn.setAttribute('disabled', true);
   }
 }
 
 userId.addEventListener('keyup', handleInput);
 userPw.addEventListener('keyup', handleInput);
+
