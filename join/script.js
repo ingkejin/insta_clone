9da645a9a67ff8d96dea4348b6694ff11268e316
ingkejin <ingkejin@gmail.com>
2024-06-14 10:41:03 +0900

    ✨ allTrue일 때 joinBtn 활성화

--- join/script.js
+++ join/script.js
@@ -12,11 +12,11 @@ let userPw = document.getElementById('user-pw');
 function updateInputState(val, activeVar) {
   if(val.value.trim().length > 0) {
     // animate-input 에 active class add
-    val.parentElemet.classList.add('active');
+    val.parentElement.classList.add('active');
     activeVar = true
   } else {
     // animate-input 에 active class remove
-    val.parentElemet.classList.remove('active');
+    val.parentElement.classList.remove('active');
     activeVar = false;
   }
 
@@ -24,11 +24,28 @@ function updateInputState(val, activeVar) {
 }
 
 
-
 animateInput.forEach((item) => {
   let input = item.querySelector('input');
   
   input.addEventListener('keyup', () => {
-    
+    if(input == userEmail) {
+      emailAct = updateInputState(input, emailAct);
+    } else if(input == userName) {
+      nameAct = updateInputState(input, nameAct);
+    } else if(input == userId) {
+      idAct = updateInputState(input, idAct); 
+    } else if(input == userPw) {
+      pwAct = updateInputState(input, pwAct);
+    }
+
+    let allTrue = emailAct && nameAct && idAct && pwAct;
+    if(allTrue) {
+      // allTrue 안 값이 모두 참일 때 
+      // joinBtn disabled 속성 remove
+      joinBtn.removeAttribute('disabled');
+      // joinBtn.setAttribute('disabled', false);
+    } else {
+      joinBtn.setAttribute('disabled', true);
+    }
   })
 })
